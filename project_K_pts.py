38d6c41a7ee883f9d9300562d01387e0e7f55191
Noah LeFrancois <49576860+nlefrancois6@users.noreply.github.com>
2025-07-08 00:49:28 -0400

    Set filename for results output dynamically

--- project_K_pts.py
+++ project_K_pts.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import k_ppf
-import nfl_data_py as nfl
 
 print('Loading player & defense data')
 # Load the player & defense data
@@ -10,7 +9,7 @@ df_def = pd.read_csv('Data/KDEF_stats_2024.csv')
 print('Predicting player performance (All matchups)')
 # Step 1: Load the current week's schedule
 yr = 2024 #Year
-wk = 10 #Week
+wk = 12 #Week
 week_schedule = k_ppf.get_weekly_schedule(year=yr, week=wk)
 print('Schedule for,',yr,'Week',wk)
 print(week_schedule.head()) #Note: stadium info available here
@@ -21,11 +20,11 @@ matchups = k_ppf.generate_weekly_matchups(df_k, week_schedule)
 # Step 3: Run projections
 weekly_kicker_projections = k_ppf.compare_kicker_matchups(matchups, df_k, df_def)
 
-# Step 4: Display or export
+# Step 4: Display & export
 print(weekly_kicker_projections.iloc[0:10])
 save_files=True
 if save_files:
     print('Saving kicker & matchup statistics to csv')
     #TODO: set filename dynamically with week value
-    weekly_kicker_projections.to_csv("week10_kicker_projections.csv", index=False)
+    weekly_kicker_projections.to_csv(f"Data/week{wk}_kicker_projections.csv", index=False)
     print('Files saved')
